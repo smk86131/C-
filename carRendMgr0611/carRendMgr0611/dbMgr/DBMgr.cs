64e3fcd4ec47b9187eeddb10e892512e9480f7e6
smk86131 <81944293+smk86131@users.noreply.github.com>
2021-06-11 16:33:10 +0900

    Add files via upload

---
+++ carRendMgr0611/carRendMgr0611/dbMgr/DBMgr.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carRendMgr0611.dbMgr
+{
+    class DBMgr
+    {
+    }
+}
---
+++ carRendMgr0611/carRendMgr0611/main/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carRendMgr0611
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //요구분석사항 - 설계 - 구현 - 테스트
+            //UML - usercase diagram - 클래스 diagram - 스퀀스 다이어그램
+            //고객 - 고객정보 제공
+            //직원 - 고객정보 등록
+            //차량 - 차량정보 제공
+            //관리자 - 단가 결정, 일일매출 조회 - (include) - 로그인
+
+            //화면설계, 클래스설계, DB설계(BRD)
+        }
+    }
+}
