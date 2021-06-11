180414986575d6b48d8536674ac250dc11a8a50e
smk86131 <81944293+smk86131@users.noreply.github.com>
2021-06-11 16:33:35 +0900

    Add files via upload

---
+++ singleTest0611/singleTest0611/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleTest0611
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                SingleTest st1 = new SingleTest(r);
+                Console.WriteLine("인스턴스: " + st1.getData()); //getData=데이터출력
+
+                //Console.WriteLine(new SingleTest().getData());
+                Console.WriteLine("싱글톤: " + SingleTest.getInst(r).getData());
+            }
+        }
+    }
+}
---
+++ singleTest0611/singleTest0611/SingleTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleTest0611
+{
+    class SingleTest
+    {
+        private static SingleTest inst;
+        private int data;
+        //static Random r = new Random(); //메인에서 랜덤쓸때
+
+        public SingleTest(Random r) //생성자의 매개변수
+        {
+            data = r.Next(1, 100);
+        }
+
+        public int getData() //int=인스턴스매서드
+        {
+            return data;
+        }
+
+        public static SingleTest getInst(Random r) //static=클래스매서드
+        {
+            if (inst == null)
+            {
+                inst = new SingleTest(r);
+            }
+            return inst;
+        }
+    }
+}
