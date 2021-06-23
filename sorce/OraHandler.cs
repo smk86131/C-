e6fda5bd7587e20eadfe602e73c40f1b6fb30d88
smk86131 <81944293+smk86131@users.noreply.github.com>
2021-06-23 16:15:05 +0900

    Add files via upload

---
+++ sorce/OraHandler.cs
@@ -0,0 +1,77 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carFixMgr0611.handler
+{
+    class OraHandler
+    {
+        const string ORADB =
+            "Data Source=(DESCRIPTION=(ADDRESS_LIST=" +
+            "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)" +
+            "(PORT=1521)))" +
+            "(CONNECT_DATA=(SERVER=DEDICATED)" +
+            "(SERVICE_NAME=XE)));" +
+            "User Id=sqlDB;Password=1234;";
+        OracleConnection conn;
+        OracleCommand cmd;
+
+        public OraHandler()
+        {
+            conn = new OracleConnection(ORADB);
+            cmd = new OracleCommand();
+            dbConnect();
+        }
+
+        ~OraHandler()
+        {
+            dbClose();
+        }
+        
+        public void dbConnect()
+        {
+            try
+            {
+                conn.Open();
+                Console.WriteLine("오라클 접속 성공!");
+            }
+            catch(OracleException e)
+            {
+                Console.WriteLine("오라클 접속 에러!");
+            }
+        }
+
+        public void dbClose()
+        {
+            try
+            {
+                conn.Close();
+                Console.WriteLine("오라클 접속 해제!");
+            }
+            catch (OracleException e)
+            {
+                Console.WriteLine("오라클 접속 해제 에러!");
+            }
+        }
+
+        public void insertDb()
+        {
+            string model = "그랜저";
+            string cc = "2500cc이하";
+            string number = "23가2345";
+            string caryear = "2020년식";
+
+            string query =
+                string.Format("insert into car_r values" +
+                "(CAR_T_SEQ.NEXTVAL, '{0}', '{1}', '{2}', '{3}')",
+                model, number, cc, caryear);
+
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
