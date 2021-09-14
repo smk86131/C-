d675d0d9c6e21d3dcabaf188978eb81ec6780d9f
smk86131 <81944293+smk86131@users.noreply.github.com>
2021-09-14 18:12:44 +0900

    Add files via upload

---
+++ Delegate_and_lamda/Delegate_and_lamda/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_and_lamda
+{
+    class Person
+    {
+
+        private string name_;
+
+        public string getName() { return name_; }
+
+        public void setName(string name) { name_ = name; }
+
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public string name2 { get; set; }
+
+        public void Sleep()
+        {
+            System.Windows.Forms.MessageBox.Show("드르르렁");
+        }
+    }
+
+    
+    
+}
